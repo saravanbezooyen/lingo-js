47f42c7953663f8d44bc1deae7bde3530b202019
Sara van Bezooyen <99021834@mydavinci.nl>
2016-12-21 09:11:44 +0100

    update

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
 function Game(naam) {
-	this.naam = naam; 
-	this.getNaam = function() {
-		return this.naam;
-	}
+	this.naam = naam;
 	this.getRandomWoord = function() {
 		return words[Math.floor((Math.random() * 479) + 1)];
-	}
+	};
+	this.randomWord = this.getRandomWoord();
+
 }
 
 var game = new Game('naam');
-console.log(game.getRandomWoord());
+var randomWord = game.RandomWoord;
+console.log(game.randomWord);
 
 //Als eerste check je of de gebruiker het woord geraden heeft. 
 //Zo niet dan kijk je of de letter er in voorkomt en mocht dat het geval zijn dan pas, of het woord ook op de juiste plek staat.
 
-function wordChecker(word) {
-	if (word == randomWord) {
+function wordChecker(word, int) {
+	if (word == game.randomWord) {
         console.log("win");
 	} else {
-		for (
-			var i = 0; 
-			i < randomWord.length; 
-			i++)
-			var res = str.charAt(i);{
-			for (
-				var x = 0; 
-				x < randomWord.length; 
-				x++){
-					if (true) {
-
-					} else {
-
-					}
-		}	
+		if (word.charAt(int) == game.randomWord.charAt(int)) {
+			return 11;
+		} else {
+			for (var i = 0; i < word.length; i++) {
+				if (word.charAt(int) == game.randomWord.charAt(i)) {
+					return 10;
+				} else {
+					return 0;
+				}
+			}
+		}
 	}
 }
+
+function layout(word) {
+	for (var i = 0; i < word.length; i++) {
+		switch(wordChecker(word, i)) {
+			case 0:
+				console.log('nee');
+				break;
+			case 11:
+				console.log('in, ja');
+				break;
+			case 10:
+				console.log('in, nee');
+				break;
+		}
+	}
 }
+
+Array.prototype.random = function() {
+    return this[Math.floor(Math.random() * this.length)];
+  }
+
+var game = new Game();
 
 /*b* a l - *b* a k
 *b* a l - b *a* k
---
+++ test.js
@@ -0,0 +1,13 @@
+var hey = 1;
+
+switch(hey) {
+	case 0:
+		console.log("Aidshond");
+		break;
+	case 1:
+		console.log("Aidskat");
+		break;
+	default:
+		console.log("Geen van beiden");
+		break;
+}
